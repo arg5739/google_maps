6b6f485d8b8314dd065f28cea7e7a0bcdfa694ab
arg5739 <arg5739@rit.edu>
2013-06-20 15:22:23 -0400

    Web service using google maps

---
+++ App_Code/IService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Configuration;
+using Subgurim;
+using Subgurim.Controles;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService" in both code and config file together.
+[ServiceContract]
+public interface IService
+{
+
+    [OperationContract]
+    GLatLng getDirection(string source, string desti);
+
+    [OperationContract]
+    GInfoWindow getMarker(string latitude, string logitude, string label);
+
+    [OperationContract]
+    GDirection getLetGO(string button_name, string start, string end, string div_direction);
+ 
+    
+	// TODO: Add your service operations here
+}
+
+// Use a data contract as illustrated in the sample below to add composite types to service operations.
+[DataContract]
+public class CompositeType
+{
+	bool boolValue = true;
+	string stringValue = "Hello ";
+
+	[DataMember]
+	public bool BoolValue
+	{
+		get { return boolValue; }
+		set { boolValue = value; }
+	}
+
+	[DataMember]
+	public string StringValue
+	{
+		get { return stringValue; }
+		set { stringValue = value; }
+	}
+}
---
+++ App_Code/Service.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Web;
+using System.Web.Script.Serialization;
+using System.Net;
+using System.Web.Services;
+using System.Configuration;
+using Subgurim;
+using Subgurim.Controles;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service" in code, svc and config file together.
+public class Service : IService
+{
+    
+    public Service()
+    {
+       
+        BasicHttpBinding httpBinding = new BasicHttpBinding();
+        httpBinding.MaxReceivedMessageSize = 2147483647;
+        httpBinding.MaxBufferSize = 2147483647;
+    }
+
+    public GLatLng getDirection(string latitude, string logitude)
+    {
+        GLatLng latLng = null;
+        //string output;
+        if (latitude.Length != 0 && logitude.Length != 0) 
+        {
+
+            using (WebClient client = new WebClient())
+            {
+                latLng = new GLatLng(Convert.ToDouble(latitude), Convert.ToDouble(logitude));
+                return (latLng);
+            }
+
+        }
+        else
+        {
+            return (latLng);
+        }
+
+        
+    }
+
+    public GInfoWindow getMarker(string latitude, string logitude, string label)
+    {
+        GLatLng latLng = null;
+        GInfoWindowOptions windowOptions;
+        GInfoWindow commonInfoWindow = null;
+        //string output;
+        if (latitude.Length != 0 && logitude.Length != 0)
+        {
+            latLng = getDirection((latitude), (logitude));
+            GMarker marker = new GMarker(latLng);
+            windowOptions = new GInfoWindowOptions();
+            commonInfoWindow = new GInfoWindow(marker, label, windowOptions);
+            return (commonInfoWindow);
+        }
+        else
+        {
+            return (commonInfoWindow);
+        }
+
+
+    }
+    public GDirection getLetGO(string button_name, string start, string end, string div_direction)
+    {
+        GDirection direction = new GDirection();
+        direction.autoGenerate = false;
+        direction.buttonElementId = button_name;
+        direction.fromElementId = start;
+        direction.toElementId = end;
+        direction.divElementId = div_direction;
+        direction.clearMap = true;
+
+        //direction.avoidHighways = true;
+        //direction.travelMode = GDirection.GTravelModeEnum.G_TRAVEL_MODE_WALKING;
+        //direction.locale = "en";
+
+        return(direction);
+
+    }
+
+
+}
